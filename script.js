a571c077535b7f98d7744549c64392bbb6b3a0e0
BelaSchrammDev <belaschramm@aol.de>
2023-11-15 21:36:38 +0100

    showquestion added

--- script.js
+++ script.js
@@ -3,9 +3,77 @@ const audioFail = new Audio('./audio/fail.mp3');
 
 let currentQuizSelect = null;
 let currentQuiz = null;
+let currentQuizQuestion = 0;
 
 function init() {
     fillQuizBoxSideBar();
+}
+
+
+function hideContainer(divID) { document.getElementById(divID).classList.add('hide'); }
+
+
+function showContainer(divID) { document.getElementById(divID).classList.remove('hide'); }
+
+
+function isQuizEnd() { return currentQuiz.questions.length <= currentQuizQuestion + 1; }
+
+
+function addIndexToQuiz(quiz, index) { quiz.index = index; }
+
+
+function replaceTags(text) {
+    let returnText = text.replaceAll('<', '&lt;');
+    returnText = returnText.replaceAll('>', '&gt;');
+    return returnText;
+}
+
+
+function clickQuizStart() {
+    if (currentQuizSelect != null) {
+        hideContainer('quiz_start');
+        showContainer('quiz_question_and_answers');
+        initQuiz();
+        showQuestion();
+    }
+}
+
+
+function initQuiz() {
+    currentQuiz = currentQuizSelect;
+    currentQuizQuestion = 0;
+}
+
+
+function showQuestion() {
+    let quest = currentQuiz.questions[currentQuizQuestion];
+    let answerHtml = '';
+    document.getElementById('quiz_question').innerHTML = replaceTags(quest.question);
+    for (let index = 0; index < quest.answers.length; index++) {
+        answerHtml += getAnswerHtml(index, replaceTags(quest.answers[index]));
+    }
+    document.getElementById('quiz_answers').innerHTML = answerHtml;
+}
+
+
+function clickNextButton() {
+    if (isQuizEnd()) {
+
+    } else {
+        currentQuizQuestion++;
+        showQuestion();
+    }
+}
+
+
+function getAnswerHtml(index, answer) {
+    const charArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G',];
+    return `
+        <div class="quiz_answer">
+        <span>${charArray[index]}</span>
+        <span>${answer}</span>
+        </div>
+    `;
 }
 
 
@@ -23,9 +91,9 @@ function fillQuizBoxSideBar() {
 }
 
 
-function setQuizDescription(quiz){
-    document.getElementById('quiz_description_tag').innerHTML=quiz.quizTag;
-    document.getElementById('quiz_description_long').innerHTML=quiz.quizName;
+function setQuizDescription(quiz) {
+    document.getElementById('quiz_description_tag').innerHTML = quiz.quizTag;
+    document.getElementById('quiz_description_long').innerHTML = quiz.quizName;
 }
 
 
@@ -47,4 +115,3 @@ function clickQuizType(index) {
 }
 
 
-function addIndexToQuiz(quiz, index) { quiz.index = index; }
